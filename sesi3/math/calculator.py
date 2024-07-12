51ffff57541903cd19b5b668db2ebb0eb09a24ae
calmantara <calmantarasp@gmail.com>
2024-07-12 21:12:19 +0700

    feat: add sesi 3

---
+++ sesi3/math/calculator.py
@@ -0,0 +1,9 @@
+def mul(a: int, b: int) -> int:
+    return b * a
+
+
+def div(a: int, b: int) -> int:
+    return b / a
+
+
+print(mul(3, 4))
---
+++ sesi3/student.py
@@ -0,0 +1,6 @@
+def register(name: str):
+    print("student", name, "registered")
+    
+    
+def check(name: str):
+    print("student", name, "registered")
